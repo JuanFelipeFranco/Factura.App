0a0377f6537bf180b40fe4bf7753150c6f100e94
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-12 11:39:04 -0500

    Create PropertySource and AppConfig main class
    creamos el archivo data.properties.
    creamos la clase appConfig que esta a nivel de clase main.

---
+++ Factura/src/main/java/com/felipe/di/Factura/AppConfig.java
@@ -0,0 +1,10 @@
+package com.felipe.di.Factura;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:data.properties")
+public class AppConfig {
+
+}
